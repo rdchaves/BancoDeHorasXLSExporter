82629999d636933ce509103b614ed84098f4f1b2
Rodrigo Chaves <rdchaves@gmail.com>
2014-04-08 09:51:35 -0300

    Formatação

--- src/br/eti/rdchaves/bancodehorasxlsexporter/control/activity/ShareActivity.java
+++ src/br/eti/rdchaves/bancodehorasxlsexporter/control/activity/ShareActivity.java
@@ -23,68 +23,69 @@ import br.eti.rdchaves.bancodehorasxlsexporter.business.reader.impl.LineSplitRea
 
 public class ShareActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
 
-	super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
 
-	// Get intent, action and MIME type
-	Intent sourceIntent = getIntent();
-	String action = sourceIntent.getAction();
-	String type = sourceIntent.getType();
-	Uri uri = sourceIntent.getParcelableExtra(Intent.EXTRA_STREAM);
+		// Get intent, action and MIME type
+		Intent sourceIntent = getIntent();
+		String action = sourceIntent.getAction();
+		String type = sourceIntent.getType();
+		Uri uri = sourceIntent.getParcelableExtra(Intent.EXTRA_STREAM);
 
-	if (uri != null) {
-	    if (Intent.ACTION_SEND.equals(action) && (type != null) && "text/csv".equals(type)) {
-		try {
+		if (uri != null) {
+			if (Intent.ACTION_SEND.equals(action) && (type != null) && "text/csv".equals(type)) {
+				try {
 
-		    File targetFile = convertFile(uri);
-		    targetFile.deleteOnExit();
+					File targetFile = convertFile(uri);
+					targetFile.deleteOnExit();
 
-		    Intent targetIntent = new Intent(Intent.ACTION_SEND);
-		    targetIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(targetFile));
-		    targetIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-		    targetIntent.setType("application/vnd.ms-excel");
-		    startActivity(Intent.createChooser(targetIntent, "Como deseja compartilhar?"));
-		} catch (Exception e) {
-		    Toast.makeText(getApplicationContext(), "", Toast.LENGTH_LONG).show();
-		    e.printStackTrace();
-		    finish();
+					Intent targetIntent = new Intent(Intent.ACTION_SEND);
+					targetIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(targetFile));
+					targetIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+					targetIntent.setType("application/vnd.ms-excel");
+					startActivity(Intent.createChooser(targetIntent, "Como deseja compartilhar?"));
+				} catch (Exception e) {
+					Toast.makeText(getApplicationContext(), "", Toast.LENGTH_LONG).show();
+					e.printStackTrace();
+					finish();
+				}
+			} else {
+				Toast.makeText(getApplicationContext(), "Other files: " + type, Toast.LENGTH_SHORT).show();
+			}
+		} else {
+			Toast.makeText(getApplicationContext(), "Arquivo CSV não recebido", Toast.LENGTH_LONG).show();
 		}
-	    } else {
-		Toast.makeText(getApplicationContext(), "Other files: " + type, Toast.LENGTH_SHORT).show();
-	    }
-	} else {
-	    Toast.makeText(getApplicationContext(), "Arquivo CSV não recebido", Toast.LENGTH_LONG).show();
 	}
-    }
 
-    private File convertFile(Uri uri) throws FileNotFoundException, Exception {
-	Reader reader = null;
-	File targetFile = null;
-	try {
-	    ParcelFileDescriptor parcelFileDescriptor = getContentResolver().openFileDescriptor(uri, "r");
-	    FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
-	    reader = new FileReader(fileDescriptor);
-	    List<Date> checkpoints = new LineSplitReader().read(reader, ",");
-	    targetFile = new File(Environment.getExternalStorageDirectory(), uri.getLastPathSegment().replace(".csv", ".xls"));
-	    new ExcelConverter<Date>().convert(targetFile, checkpoints);
-	} catch (CSVReadFailException e) {
-	    Toast.makeText(getApplicationContext(), "Falha na leitura do arquivo: formato incorreto", Toast.LENGTH_LONG)
-		    .show();
-	    throw e;
-	} finally {
-	    IOUtils.closeQuietly(reader);
+	private File convertFile(Uri uri) throws FileNotFoundException, Exception {
+		Reader reader = null;
+		File targetFile = null;
+		try {
+			ParcelFileDescriptor parcelFileDescriptor = getContentResolver().openFileDescriptor(uri, "r");
+			FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+			reader = new FileReader(fileDescriptor);
+			List<Date> checkpoints = new LineSplitReader().read(reader, ",");
+			targetFile = new File(Environment.getExternalStorageDirectory(), uri.getLastPathSegment().replace(".csv",
+					".xls"));
+			new ExcelConverter<Date>().convert(targetFile, checkpoints);
+		} catch (CSVReadFailException e) {
+			Toast.makeText(getApplicationContext(), "Falha na leitura do arquivo: formato incorreto", Toast.LENGTH_LONG)
+					.show();
+			throw e;
+		} finally {
+			IOUtils.closeQuietly(reader);
+		}
+		return targetFile;
 	}
-	return targetFile;
-    }
 
-    /** Checks if external storage is available for read and write */
-    public boolean isExternalStorageWritable() {
-	String state = Environment.getExternalStorageState();
-	if (Environment.MEDIA_MOUNTED.equals(state)) {
-	    return true;
+	/** Checks if external storage is available for read and write */
+	public boolean isExternalStorageWritable() {
+		String state = Environment.getExternalStorageState();
+		if (Environment.MEDIA_MOUNTED.equals(state)) {
+			return true;
+		}
+		return false;
 	}
-	return false;
-    }
 }
